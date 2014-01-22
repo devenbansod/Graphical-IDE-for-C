0250f4ed3d273332b74e2367c4ed4b2fd8b5a299
devenbansod <devenbansod.bits@gmail.com>
2014-01-22 16:23:06 +0530

    Create script.php

---
+++ script.php
@@ -0,0 +1,19 @@
+<?php 
+  $code = trim($_POST["code"]);
+  
+  if ($code == "")                // if user id is blank
+    echo "***You must not leave it blank***";
+    
+  else{                              // if code anything else
+      $file = fopen("test.c", "w") or die("Unable to Open File");
+      system("chmod 777 test.c");
+      fwrite($file, $code) or die("Unable to Write the File");
+      passthru("gcc test.c 2>&1");
+      system("chmod 777 a.out");
+      echo "\n";
+      system("./a.out 2>&1");
+      unlink("a.out");
+      unlink("test.c");    
+  }
+
+?>
