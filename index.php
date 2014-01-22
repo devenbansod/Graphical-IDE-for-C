c93c5da0f41bb98dab08a4566add8a392271d6d7
devenbansod <devenbansod.bits@gmail.com>
2014-01-22 16:28:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <html>
+    <head>
+  <meta charset="utf-8">
+  <title>jQuery UI Draggable - Default functionality</title>
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  #draggable { width: 150px; height: 150px; padding: 0.5em; }
+  </style>
+  <script>
+  $(function() {
+    $( "#draggable" ).draggable();
+  });
+  </script>
 <script>
     function PostData() {
     // 1. Create XHR instance - Start
@@ -33,13 +48,17 @@
     // 3. Specify your action, location and Send to the server - End
 }
 </script>
-
+</head>
+<body>
+<div id="draggable" class="ui-widget-content" style="float:right">
+  <p>Drag me around</p>
+</div>
     <form>
     <label for="userid">Code:</label><br>
     <textarea name ="code" id="code" onblur="" cols ="50" rows="10"></textarea>
     <br><label for="userid">Results:</label><br>
     <br><textarea name ="results" id="results" onblur="" cols="50" rows="10"></textarea>
     <br><br><input type="button" value ="Compile and Run" onclick="PostData();" />
-    <div draggable="true" class="resizable">Hiii</div>
 </form>
+  </body>
 </html>
